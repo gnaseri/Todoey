43a1da1c57df32ebf4bea4b925f1406a1b8a5900
[gelareh] <[gelareh.naseri@gmail.com]>
2018-11-01 23:24:21 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Models/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by g on 11/1/18.
+//  Copyright © 2018 g. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
